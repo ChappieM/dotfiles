55559b69d27ab569fbc7ba7df0afaa1cfcf67fe2
chawata-ubuntu <cha39881@gmail.com>
2023-02-08 05:42:35 +0900

    プロンプト更新

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,61 +1,29 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l last_pipestatus $pipestatus
-    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+set __fish_git_prompt_showdirtystate yes
+set __fish_git_prompt_showstashstate yes
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showupstream yes
+set __fish_git_prompt_color_branch magenta
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_char_untrackedfiles "?"
+set __fish_git_prompt_char_upstream_equal ""
+set __fish_git_prompt_char_stagedstate "+"
+set __fish_git_prompt_char_dirtystate "!"
+set __fish_git_prompt_char_stateseparator " "
 
-    if not set -q __fish_git_prompt_show_informative_status
-        set -g __fish_git_prompt_show_informative_status 1
+function fish_prompt
+    set -l p (set_color $fish_color_cwd)(prompt_pwd)(set_color normal)
+    set -l g (__fish_git_prompt "%s")
+    if test -n "$g"
+      set -l gg (string split " " -- $g)
+      set -a p " "$gg[1]
+      set -l ggg $gg[2]
+      if test -n "$ggg"
+        set -a p (printf " %s[%s]%s" (set_color red) $ggg (set_color normal))
+      end
     end
-    if not set -q __fish_git_prompt_hide_untrackedfiles
-        set -g __fish_git_prompt_hide_untrackedfiles 1
-    end
-    if not set -q __fish_git_prompt_color_branch
-        set -g __fish_git_prompt_color_branch magenta --bold
-    end
-    if not set -q __fish_git_prompt_showupstream
-        set -g __fish_git_prompt_showupstream informative
-    end
-    if not set -q __fish_git_prompt_color_dirtystate
-        set -g __fish_git_prompt_color_dirtystate blue
-    end
-    if not set -q __fish_git_prompt_color_stagedstate
-        set -g __fish_git_prompt_color_stagedstate yellow
-    end
-    if not set -q __fish_git_prompt_color_invalidstate
-        set -g __fish_git_prompt_color_invalidstate red
-    end
-    if not set -q __fish_git_prompt_color_untrackedfiles
-        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-    end
-    if not set -q __fish_git_prompt_color_cleanstate
-        set -g __fish_git_prompt_color_cleanstate green --bold
-    end
+    set -a p \n
+    set -a p (set_color $fish_color_ps)'➜'(set_color normal)" "
 
-    set -l color_cwd
-    set -l suffix
-    if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        else
-            set color_cwd $fish_color_cwd
-        end
-        set suffix '#'
-    else
-        set color_cwd $fish_color_cwd
-        set suffix '$'
-    end
-
-    # PWD
-    set_color $color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (fish_vcs_prompt)
-
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color --bold $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-    echo -n $prompt_status
-    set_color normal
-
-    echo -n "$suffix "
+    echo -ns $p
 end
