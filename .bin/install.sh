88764bbf86162fda326ca47d3ed334d113b4f4cc
chawata-ubuntu <cha39881@gmail.com>
2023-02-08 03:28:49 +0900

    dotfilesツール導入

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+cd `dirname $0`
 
 # Install fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
 
-
 # シンボリックリンクの作成
-DOT_FILES=".gitconfig .zshrc .zprofile"
-for file in $DOT_FILES
-do
-    ln -sf `pwd`/$file ~
-done
+pip install dotfiles
+cd ../../
+dotfiles -C "dotfiles/.dotfilesrc" --sync
