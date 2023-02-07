66fc34466cc21a63028de85568165b36064a3031
chawata-ubuntu <cha39881@gmail.com>
2023-02-08 03:47:28 +0900

    install pip

--- .bin/install.sh
+++ .bin/install.sh
@@ -4,9 +4,10 @@ cd `dirname $0`
 # Install fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt install -y fish
 
 # シンボリックリンクの作成
+sudo apt install -y python3-pip
 pip install dotfiles
 cd ../../
 dotfiles -C "dotfiles/.dotfilesrc" --sync
