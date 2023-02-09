c79cec4a6f995bdbdab16ca03ae099bb26d7ef55
chawata-ubuntu <cha39881@gmail.com>
2023-02-09 13:24:15 +0900

    add alias.fish

---
+++ .config/fish/alias.fish
@@ -0,0 +1,8 @@
+# git
+abbr -a lg lazygit
+abbr -a gc git commit
+abbr -a gco git checkout
+abbr -a gp git push
+
+# docker 
+abbr -a dc docker compose
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    abbr -a lg lazygit
-    abbr -a gc git commit
-    abbr -a gco git checkout
-    abbr -a gp git push
-
-    abbr -a dc docker compose
 end
 
 # tmux自動起動
