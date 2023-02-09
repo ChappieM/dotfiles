2b095badd0a36fec6d5c3249903de27fee222293
chawata-ubuntu <cha39881@gmail.com>
2023-02-10 00:39:16 +0900

    Change apt tp apt-get

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,18 +2,18 @@
 cd $HOME
 
 # setup
-sudo apt install -y curl software-properties-common
+sudo apt-get install -y curl software-properties-common
 
 # Install fish
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install -y fish
+sudo apt-get update
+sudo apt-get install -y fish
 
 # Install StarShip
 curl -sS https://starship.rs/install.sh | sh
 
 # Install tmux
-sudo apt install -y tmux
+sudo apt-get install -y tmux
 
 # 既存の.config, .bin内のファイルを取り込む
 if [ -d "$HOME/.config/" ]; then
@@ -24,7 +24,7 @@ if [ -d "$HOME/.bin/" ]; then
 fi
 
 # シンボリックリンク作成
-sudo apt install -y python3-pip
+sudo apt-get install -y python3-pip
 pip install dotfiles
 export PATH=$PATH:~/.local/bin
 
@@ -32,5 +32,5 @@ dotfiles -C "$HOME/dotfiles/.dotfilesrc" --sync --force
 
 # Install fish plugins with fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-sudo apt install -y fzf
+sudo apt-get install -y fzf
 fish -c "fisher update"
