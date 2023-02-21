cf60a207c6c2ea111d35dcafdc40c9800a1a057b
chawata-ubuntu <cha39881@gmail.com>
2023-02-22 03:03:22 +0900

    Remove some dead code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,8 +19,8 @@ require("mason-lspconfig").setup()
 local lsp = require("lspconfig")
 -- lsp.pyright.setup({})
 -- lsp.tsserver.setup({})
-require("lspconfig").lua_ls.setup({
-				settings = {
+lsp.lua_ls.setup({
+	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
@@ -87,34 +87,14 @@ null_ls.setup({
 })
 
 -- Formatter
--- Utilities for creating configurations
 local util = require("formatter.util")
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup({
-	-- Enable or disable logging
 	logging = true,
-	-- Set the log level
 	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
 	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
 		lua = {
-			-- "formatter.filetypes.lua" defines default configurations for the
-			-- "lua" filetype
-			-- デフォルトの設定もあるが、設定が不透明になるので今回は使わない
-			-- require("formatter.filetypes.lua").stylua,
-
-			-- You can also define your own configuration
 			function()
-				-- Supports conditional formatting
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
-
-				-- Full specification of configurations is down below and in Vim help
-				-- files
 				return {
 					exe = "stylua",
 					args = {
@@ -128,12 +108,7 @@ require("formatter").setup({
 				}
 			end,
 		},
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
 		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
 			require("formatter.filetypes.any").remove_trailing_whitespace,
 		},
 	},
