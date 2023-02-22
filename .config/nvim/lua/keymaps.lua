964828a91e5ea8012f8c4de9b7cdb85f6cd6a41c
chawata-ubuntu <cha39881@gmail.com>
2023-02-23 05:33:20 +0900

    Make setting file divide

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,21 +6,28 @@
 --   term_mode = 't',
 --   command_mode = 'c',
 
-
 vim.g.mapleader = " "
 
-vim.keymap.set("i","jk","<Esc>", { noremap = true, silent = true})
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", ";", ":", { noremap = true })
+vim.keymap.set("n", "x", '"_x', { noremap = true })
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
 
--- ファイラー 
-vim.keymap.set("n", "<leader>b",":Telescope file_browser<CR>",{ noremap = true })
+-- ファイラー
+vim.keymap.set("n", "<leader>b", ":Telescope file_browser<CR>", { noremap = true })
 
 -- fzf
-vim.keymap.set("n", "<leader>e",":Telescope find_files<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>e", ":Telescope find_files<CR>", { noremap = true })
 
 -- バッファ切り替え
-vim.keymap.set("n", "<C-n>",":bnext<CR>", { noremap = true })
-vim.keymap.set("n", "<C-p>",":bprev<CR>", { noremap = true })
+vim.keymap.set("n", "<C-n>", ":bnext<CR>", { noremap = true })
+vim.keymap.set("n", "<C-p>", ":bprev<CR>", { noremap = true })
 
--- 保存
-vim.keymap.set("n","<leader>w",":w<CR>", { noremap = true})
+-- フォーマットして保存
+vim.keymap.set("n", "<leader>w", ":FormatWrite<CR>", { noremap = true })
 
+-- LSP
+
+-- hop
+vim.keymap.set("n", "<leader><leader>", ":HopChar1<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>l", ":HopLine<CR>", { noremap = true })
